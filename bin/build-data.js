ed781afb4c9c30165bfef5882011627305897fa4
Kasper Jacobsen <k@mackwerk.dk>
2017-07-06 20:34:53 +0200

    add shortname to unicode

--- bin/build-data.js
+++ bin/build-data.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const util = require('util');
 const path = require('path');
 const data = require('emoji-datasource');
+const emojione = require('emojione');
 
 console.log('[emoji-short] Generating data.');
 const result = data
@@ -9,12 +10,13 @@ const result = data
   .reduce((prev, curr) => {
     const [ shortnames, image ] = curr.split('@');
     shortnames.split('|').forEach(shortname => {
-      prev[shortname] = image;
+      prev[shortname] = { image: image, unicode: emojione.convert(image.replace('.png', '')) };
     });
     return prev;
   }, {});
 
 console.log('[emoji-short] Writing data to file.');
-fs.writeFileSync(path.join(process.cwd(), 'data/data.js'), `module.exports = ${util.inspect(result, { breakLength: Infinity })};`);
+const writePath = path.join(process.cwd(), 'data/data.js');
+fs.writeFileSync(writePath, `module.exports = ${util.inspect(result, { breakLength: Infinity })};\n`);
 
 console.log('[emoji-short] Done :white_check_mark:');
--- index.js
+++ index.js
@@ -5,22 +5,33 @@ class EmojiShort {
     this.cdnUrl = cdnUrl;
     this.klass = klass;
 
-    this.shortnamePattern = Object.keys(emojiMap).join('|').replace(/\+/g, '\\+');
-    this.pattern = new RegExp(this.shortnamePattern, 'gi');
+    this.shortnames = Object.keys(emojiMap).join('|').replace(/\+/g, '\\+');
+    this.shortnamePattern = new RegExp(this.shortnames, 'gi');
+
+    this.unicodes = Object.keys(emojiMap).map(k => emojiMap[k].unicode).join('|');
 
     this._replaceShortNameWithImgTag = this._replaceShortNameWithImgTag.bind(this);
+    this._replaceShortNameWithUnicode = this._replaceShortNameWithUnicode.bind(this);
 
     this.toImage = this.toImage.bind(this);
   }
 
   toImage (string) {
-    return string.replace(this.pattern, this._replaceShortNameWithImgTag);
+    return string.replace(this.shortnamePattern, this._replaceShortNameWithImgTag);
+  }
+
+  toUnicode (string) {
+    return string.replace(this.shortnamePattern, this._replaceShortNameWithUnicode);
   }
 
   _replaceShortNameWithImgTag (shortname) {
-    const image = emojiMap[shortname];
+    const image = emojiMap[shortname].image;
     if (!image) return shortname;
     return `<img class="${this.klass}" src="${this.cdnUrl}${image}" alt="${shortname}" title="${shortname}" />`;
+  }
+
+  _replaceShortNameWithUnicode (shortname) {
+    return emojiMap[shortname].unicode;
   }
 }
 
